3cf98eda6c5e199afc9d98461416afde6399fc08
Werner gehrke Junior <werner.junior@hbsis.com.br>
2022-01-26 14:03:25 -0300

    run file

---
+++ run.js
@@ -0,0 +1,5 @@
+const script = document.createElement("script")
+script.type = "text/javascript"
+script.src = "https://cdn.jsdelivr.net/gh/wernerjr/zodiacsBot@main/ZodiacsBot.js"
+document.head.appendChild(script)
+eval(script);
